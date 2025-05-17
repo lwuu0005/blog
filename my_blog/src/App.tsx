ee820e28d764daed6ee70948e69bac3e8ca37d64
lwuu0005 <lwuu0005@student.monash.edu>
2025-05-18 00:05:24 +1000

    bug_fix

--- my_blog/src/App.tsx
+++ my_blog/src/App.tsx
@@ -30,11 +30,11 @@ export default function Resume() {
                   <span className="i-mdi-phone mr-2"></span>
                   {/* 010-1234-1234 */}
                 </div>
-                <div class="flex items-center space-x-2">
-                  <svg class="w-5 h-5 text-gray-600" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                <div className="flex items-center space-x-2">
+                  <svg className="w-5 h-5 text-gray-600" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path fill-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.113.82-.26.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.385-1.333-1.753-1.333-1.753-1.09-.745.083-.729.083-.729 1.205.085 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.997.108-.775.418-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23a11.5 11.5 0 013.005-.405c1.02.005 2.045.138 3.005.405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.605-.015 2.895-.015 3.285 0 .315.21.69.825.57C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z" clip-rule="evenodd"/>
                   </svg>
-                  <a href="https://github.com/lwuu0005/" class="font-black hover:underline" target="_blank" rel="noopener noreferrer">
+                  <a href="https://github.com/lwuu0005/" className="font-black hover:underline" target="_blank" rel="noopener noreferrer">
                     https://github.com/lwuu0005/
                   </a>
                 </div>
@@ -217,7 +217,7 @@ function ExperienceItem({
   title: string; 
   company: string; 
   period: string; 
-  description: string 
+  description: React.ReactNode;
 }) {
   return (
     <div className="border-l-4 border-blue-500 pl-4">
